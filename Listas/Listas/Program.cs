ed44f363360ba6faaa87befc4de0da12587e0935
Crislaine Paula <96801822+CrislainePaula@users.noreply.github.com>
2022-04-05 03:33:19 -0300

    first commit

---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic; //serve para trabalhar com listas
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> clientes = new List<string>();
+            clientes.Add("Pandoro");
+            clientes.Add("Pacheco");
+            string pessoa = "José";
+            clientes.Add(pessoa);
+
+            foreach(string cliente in clientes)//mostrar todos os itens
+            {
+                Console.WriteLine(cliente);
+            }
+
+            //string busca = clientes.Find(cliente => cliente == "Pandoro");
+            //string busca = clientes.Find(cliente => cliente.Length > 4);//buscando na lista um nome que tenha mais do que 4 carctere
+            List<string> filtragem = clientes.FindAll(cliente => cliente.Length < 5);//filtra todos da lista que sejam menor que 5
+            Console.WriteLine("========");
+
+            foreach(string cliente in filtragem)
+            {
+                Console.WriteLine(cliente);
+            }
+
+            /*clientes.RemoveAll( cliente => cliente == "Pandoro");//remove um item quando você não sabe a posição do item na lista
+
+            clientes.RemoveAt(1);//remove o item pela posição
+            Console.WriteLine("============");
+
+            foreach (string cliente in clientes)
+            {
+                Console.WriteLine(cliente);
+            }
+
+            clientes[2] = "Crislaine"; //editar um item
+            Console.WriteLine(clientes[2]);*/
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Structs/AulaStructs/AulaStructs/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaStructs
+{
+    internal class Program
+    {
+
+        struct Produto
+        {
+            public string nome;
+            public float preco;
+            public float peso;
+            public string marca;
+
+            public Produto(string nome, float preco, float peso, string marca)
+            {
+                this.nome = nome;
+                this.preco = preco;
+                this.peso = peso;
+                this.marca = marca;
+            }
+
+            public void ExibirInfo()
+            {
+                Console.WriteLine($"Nome: {this.nome}");
+                Console.WriteLine($"Preço: R${this.preco}");
+                Console.WriteLine($"Peso: {this.peso}KG");
+                Console.WriteLine($"Marca: {this.marca}");
+            }
+
+            public float AdicionarCupom(float porc)
+            {
+                float desconto = this.preco * porc / 100f;
+                return this.preco - desconto;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Produto bola = new Produto("Bola de futebol",100f,1f,"Nike");
+            Produto balde = new Produto("Balde de plastico",200f,2f,"Plastic");
+
+            bola.ExibirInfo();
+            Console.WriteLine("_______________");
+            balde.ExibirInfo();
+
+            float valorfinal = bola.AdicionarCupom(50f);
+            balde.AdicionarCupom(50f);
+
+            Console.ReadLine();
+
+        }
+    }
+}
